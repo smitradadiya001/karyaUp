9c4dc3e0473eab8d2e2ddd960e225a24c3c746b2
karyaup-8999 <karyaaup@gmail.com>
2026-04-08 16:26:00 +0530

    8/04(final)

--- src/pages/features/AIAgents.jsx
+++ src/pages/features/AIAgents.jsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion";
-import { Sparkles, Check, Bot } from "lucide-react";
-import FeatureCTA from "../../components/FeatureCTA";
+
+
+
 import { Helmet } from "react-helmet-async";
-import FeatureStack from "../../components/FeatureStack";
+
 import PageHero from "../../components/PageHero";
 
 // You can replace this with your actual image
@@ -22,7 +21,7 @@ export default function AIAgents() {
 
       <div style={{ width: "100%", height: "100vh", overflow: "hidden", margin: 0, padding: 0 }}>
         <iframe
-          src="/public/New folder/New folder/index.html"
+          src="/New folder/New folder/Agent.html"
           title="Demo Page"
           style={{
             width: "100%",
